205aed7c9ea60c5f2815f15995e8445f4c2ed882
Govind <gvind4@gmail.com>
2020-08-20 19:23:11 +0530

    got express template to working format

--- templates/express-js-sequelize/src/index.js
+++ templates/express-js-sequelize/src/index.js
@@ -1,8 +1,40 @@
-const app = require('./config/app');
+'use strict';
+const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const routerConfig = require('./modules/index');
+// TODO: add database configurations.
+const config = require('./config/config');
+const logger = require('./helpers/logger');
+const port = config.PORT || 3000;
 
-(function() {
-  // TODO:
-  // add ENV config here.
-  
-  app.init();
-})();
+const init = () => {
+  // *** express instance *** //
+  const app = express();
+  // Configuraing the standard middlewares.
+  setupStandardMiddlewares(app);
+  configureApiEndpoints(app);
+  app.listen(port);
+  console.log(`App listening in port : ${port}`);
+  logger.info(`App listening in port : ${port}`);
+};
+
+const setupStandardMiddlewares = (app) => {
+  // parse requests of content-type - application/json
+  app.use(bodyParser.json());
+  // parse requests of content-type - application/x-www-form-urlencoded
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cors());
+  return;
+};
+
+const configureApiEndpoints = (app) => {
+  app.use("/api/v1/", routerConfig.init());
+  // routerConfig.init(app);
+  // define a route handler for the default home page
+  app.get( "/", (req, res) => {
+    res.send( "Welcome to Invoice generator application! " );
+  });
+};
+
+init();
