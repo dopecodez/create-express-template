264b100ec1395b799cfd6c37f812417adfe79c00
Govind <gvind4@gmail.com>
2020-08-22 11:51:13 +0530

    added express js template

---
+++ templates/express-js/src/modules/index.js
@@ -0,0 +1,10 @@
+'use strict';
+const { Router } = require('express');
+const router = Router();
+
+const init = () => {
+    // *** register routes here *** //
+    return router;
+};
+
+module.exports = {init};
