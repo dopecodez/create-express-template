f5be63b8cd3df090190fcd5816c31a347ef5e7e0
Govind <gvind4@gmail.com>
2020-08-23 19:16:58 +0530

    commented out sequelize code

--- templates/express-js-sequelize/src/modules/user/userService.js
+++ templates/express-js-sequelize/src/modules/user/userService.js
@@ -1,12 +1,14 @@
-const User = require('../../models/user')
+//Uncomment the code in this file to see sequelize at work
+// const User = require('../../models/user')
 
 class stringCountManager {
     async getUsers() {
         // eslint-disable-next-line no-useless-catch
         try {
             //logic here
-            const jane = await User.create({ id: 1, firstName: "Jane" });
-            return jane;
+            // const jane = await User.create({ id: 1, firstName: "Jane" });
+            // return jane;
+            return "it works";
         // eslint-disable-next-line no-unreachable
         } catch (error) {
             throw error;
